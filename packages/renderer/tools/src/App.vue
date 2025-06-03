af572958f426bf7b7a4b31d592d02b5204f13844
Ne0inHK <001@zeeklog.com>
2025-06-03 16:47:40 +0800

    🖥️ Add online tools

---
+++ packages/renderer/tools/src/App.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { RouterView, useRoute } from 'vue-router';
+import { NGlobalStyle, NMessageProvider, NNotificationProvider, darkTheme } from 'naive-ui';
+import { darkThemeOverrides, lightThemeOverrides } from './themes';
+import { layouts } from './layouts';
+import { useStyleStore } from './stores/style.store';
+
+const route = useRoute();
+const layout = computed(() => route?.meta?.layout ?? layouts.base);
+const styleStore = useStyleStore();
+
+const theme = computed(() => (styleStore.isDarkTheme ? darkTheme : null));
+const themeOverrides = computed(() => (styleStore.isDarkTheme ? darkThemeOverrides : lightThemeOverrides));
+
+const { locale } = useI18n();
+
+syncRef(
+  locale,
+  useStorage('locale', locale),
+);
+</script>
+
+<template>
+  <n-config-provider :theme="theme" :theme-overrides="themeOverrides">
+    <NGlobalStyle />
+    <NMessageProvider placement="bottom">
+      <NNotificationProvider placement="bottom-right">
+        <component :is="layout">
+          <RouterView />
+        </component>
+      </NNotificationProvider>
+    </NMessageProvider>
+  </n-config-provider>
+</template>
+
+<style>
+body {
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+/* src/assets/global.css */
+:root {
+  --scrollbar-track: #f1f1f1;
+  --scrollbar-thumb: #888;
+  --scrollbar-thumb-hover: #555;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+::-webkit-scrollbar-track {
+  background: var(--scrollbar-track);
+  border-radius: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--scrollbar-thumb);
+  border-radius: 4px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: var(--scrollbar-thumb-hover);
+}
+
+html {
+  scrollbar-width: thin;
+  scrollbar-color: var(--scrollbar-thumb) var(--scrollbar-track);
+}
+</style>
